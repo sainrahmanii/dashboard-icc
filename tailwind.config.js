41be61a4df96a14a4ea9c097ce107e7baafc95d1
sainrahmanii <husainrahmani127@gmail.com>
2022-12-27 09:24:42 +0700

    change folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       height: {
